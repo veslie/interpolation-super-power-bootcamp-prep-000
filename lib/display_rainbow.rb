acef92f0b52fc9a579125c45ffcf04ee64d80117
veslie losius <veslielosius@yahoo.com>
2018-12-28 03:06:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,6 @@
 def display_rainbow
-  puts (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  puts letters = ["b","a","c"]
+puts "The first letter in the alphabet is: #{letters[1]}"
+puts "The second letter in the alphabet is: #{letters[0]}"
+puts "The third letter in the alphabet is: #{letters[2]}"
 end
