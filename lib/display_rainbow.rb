8cda60dd3d22e642106c5b79cef5301e5bc4a610
veslie losius <veslielosius@yahoo.com>
2018-12-28 03:06:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,1 @@
-def display_rainbow
-  puts letters = ["b","a","c"]
-puts "The first letter in the alphabet is: #{letters[1]}"
-puts "The second letter in the alphabet is: #{letters[0]}"
-puts "The third letter in the alphabet is: #{letters[2]}"
-end
+def 
