b85b24e2cf0f2e96cd9a1c2bc8db28a56a4c74ad
veslie losius <veslielosius@yahoo.com>
2018-12-28 03:04:05 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,4 @@
 def display_rainbow
   puts (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  puts 
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+end
